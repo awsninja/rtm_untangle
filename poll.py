58c53210eb93c79336a255cb07ffcbdb3a8ef292
Jay Muntz <jaymuntz@jaymuntz.com>
2023-07-07 11:46:13 -0400

    better logging

--- poll.py
+++ poll.py
@@ -3,6 +3,7 @@ from untangle import Untangle
 from dotenv import load_dotenv
 import os
 import logging
+from sys import stdout
 from datetime import datetime
 import time
 
@@ -11,33 +12,48 @@ load_dotenv()
 untangle = Untangle()
 rtm = RTM()
 
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
+# logging.basicConfig(
+#     format='%(asctime)s %(levelname)-8s %(message)s',
+#     level=logging.INFO,
+#     datefmt='%Y-%m-%d %H:%M:%S'
+# )
+
+# Define logger
+logger = logging.getLogger('poll_logger')
+
+logger.setLevel(logging.DEBUG) # set logger level
+logFormatter = logging.Formatter\
+("%(name)-12s %(asctime)s %(levelname)-8s %(filename)s:%(funcName)s %(message)s")
+consoleHandler = logging.StreamHandler(stdout) #set streamhandler to stdout
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
+
+
+
 class Poll:
     def run(self):
         while True:
             status_obj = untangle.firewall_get_status()
             status = status_obj['result']
-            logging.info(f"Firewall Status is {status}")
+            logger.info(f"Firewall Status is {status}")
             now  = datetime.now()
             four_am = now.replace(hour=4, minute=0, second=0, microsecond=0)
             ten_pm = now.replace(hour=22, minute=0, second=0, microsecond=0)
             if now < four_am or now > ten_pm:
                 if status != 'RUNNING':
-                    logging.info('Nighttime Sending START FIREWALL call')
+                    logger.info('Nighttime Sending START FIREWALL call')
                     untangle.firewall_start()
             else:
                 tasks_overdue = rtm.poll()
                 if tasks_overdue:
                     if status != 'RUNNING':
-                        logging.info('Sending START FIREWALL call')
+                        logger.info('Overdue Tasks Sending START FIREWALL call')
                         untangle.firewall_start()
                 else:
                     if status != 'INITIALIZED':
-                        logging.info('Sending Stop Firewall call')
+                        logger.info('Sending Stop Firewall call')
                         untangle.firewall_stop()
+            logger.info(f"Sleeping for 30 minutes")
             time.sleep(30*60)
 
 poll = Poll()
--- rtm.py
+++ rtm.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import webbrowser
+import logging
+from sys import stdout
 from datetime import datetime, timezone
 from rtmapi import Rtm
 
@@ -12,6 +14,15 @@ RTM_SHARED_SECRET = os.environ.get('RTM_SHARED_SECRET')
 RTM_TOKEN = os.environ.get('RTM_TOKEN')
 OVERDUE_TASK_FILTER = os.environ.get('OVERDUE_TASK_FILTER')
 
+logger = logging.getLogger('rtm_logger')
+logger.setLevel(logging.DEBUG) # set logger level
+logFormatter = logging.Formatter\
+("%(name)-12s %(asctime)s %(levelname)-8s %(filename)s:%(funcName)s %(message)s")
+consoleHandler = logging.StreamHandler(stdout) #set streamhandler to stdout
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
+
+
 class RTM:
 
     api = Rtm(RTM_API_KEY, RTM_SHARED_SECRET, "delete", RTM_TOKEN)
@@ -22,6 +33,7 @@ class RTM:
         for tasklist in result.tasks:
             for taskseries in tasklist:
                 if taskseries.task.completed == '':
+                    logger.info(f"Overdue Task: {taskseries.name} in list")
                     overdue = True
         return overdue
 
